5dfaade558588d99cf961f8d5e61a78e77713937
tamagoru <marushannnn@outlook.com>
2022-10-26 23:44:15 -0300

    odio todo

--- League_of_Champs/inicio.vb
+++ League_of_Champs/inicio.vb
@@ -1,11 +1,7 @@
 ﻿Public Class inicio
     Private Sub inicio_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim background As String = "fondo"
-        Dim logo As String = "logo"
+        Dim background As String = "fondo - Copy"
 
-        PictureBox2.Image = My.Resources.ResourceManager.GetObject(logo)
-
-        'Me.BackgroundImage = My.Resources.ResourceManager.GetObject(background)
-        'Me.BackgroundImageLayout = ImageLayout.Zoom
+        PictureBox2.Image = My.Resources.ResourceManager.GetObject(background)
     End Sub
 End Class
