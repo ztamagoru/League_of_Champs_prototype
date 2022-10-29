42c991d400a77b3d0303187d069b00b12bcf84d2
tamagoru <marushannnn@outlook.com>
2022-10-29 01:05:26 -0300

    registro iniciado. falta completar

--- League_of_Champs/registro.vb
+++ League_of_Champs/registro.vb
@@ -1,8 +1,9 @@
 ﻿Public Class registro
+    Dim validator As New validador
     Dim extra As New extras
 
     Private Sub _username_Enter(sender As Object, e As EventArgs) Handles _username.Enter
-        If _username.Text = "Username" And _username.ForeColor = Label1.ForeColor Then
+        If _username.Text.Trim = "Username" And _username.ForeColor = Label1.ForeColor Then
 
             _username.Text = String.Empty
             _username.ForeColor = Color.FromArgb(1, 60, 60, 65)
@@ -11,7 +12,7 @@
     End Sub
 
     Private Sub _username_Leave(sender As Object, e As EventArgs) Handles _username.Leave
-        If _username.Text = String.Empty Then
+        If _username.Text.Trim = String.Empty Then
 
             _username.Text = "Username"
             _username.ForeColor = Label1.ForeColor
@@ -20,7 +21,7 @@
     End Sub
 
     Private Sub _password_Enter(sender As Object, e As EventArgs) Handles _password.Enter
-        If _password.Text = "Password" And _password.ForeColor = Label1.ForeColor Then
+        If _password.Text.Trim = "Password" And _password.ForeColor = Label1.ForeColor Then
 
             _password.Text = String.Empty
             _password.ForeColor = Color.FromArgb(1, 60, 60, 65)
@@ -30,7 +31,7 @@
     End Sub
 
     Private Sub _password_Leave(sender As Object, e As EventArgs) Handles _password.Leave
-        If _password.Text = String.Empty Then
+        If _password.Text.Trim = String.Empty Then
 
             _password.Text = "Password"
             _password.ForeColor = Label1.ForeColor
@@ -40,8 +41,8 @@
     End Sub
 
     Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
-        Me.Hide()
         inicio.Show()
+        Me.Hide()
     End Sub
 
     Private Sub registro_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
@@ -50,5 +51,82 @@
 
     Private Sub registro_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         extra.redondearBoton(button)
+    End Sub
+
+    Private Sub _name_Enter(sender As Object, e As EventArgs) Handles _name.Enter
+        If _name.Text.Trim = "Name" And _name.ForeColor = Label1.ForeColor Then
+
+            _name.Text = String.Empty
+            _name.ForeColor = Color.FromArgb(1, 60, 60, 65)
+
+        End If
+    End Sub
+
+    Private Sub _name_Leave(sender As Object, e As EventArgs) Handles _name.Leave
+        If _name.Text.Trim = String.Empty Then
+
+            _name.Text = "Name"
+            _name.ForeColor = Label1.ForeColor
+
+        End If
+    End Sub
+
+    Private Sub _surname_Enter(sender As Object, e As EventArgs) Handles _surname.Enter
+        If _surname.Text.Trim = "Surname" And _surname.ForeColor = Label1.ForeColor Then
+
+            _surname.Text = String.Empty
+            _surname.ForeColor = Color.FromArgb(1, 60, 60, 65)
+
+        End If
+    End Sub
+
+    Private Sub _surname_Leave(sender As Object, e As EventArgs) Handles _surname.Leave
+        If _surname.Text.Trim = String.Empty Then
+
+            _surname.Text = "Surname"
+            _surname.ForeColor = Label1.ForeColor
+
+        End If
+    End Sub
+
+    Private Sub _email_Enter(sender As Object, e As EventArgs) Handles _email.Enter
+        If _email.Text.Trim = "Email" And _email.ForeColor = Label1.ForeColor Then
+
+            _email.Text = String.Empty
+            _email.ForeColor = Color.FromArgb(1, 60, 60, 65)
+
+        End If
+    End Sub
+
+    Private Sub _email_Leave(sender As Object, e As EventArgs) Handles _email.Leave
+        If _email.Text.Trim = String.Empty Then
+
+            _email.Text = "Email"
+            _email.ForeColor = Label1.ForeColor
+
+        End If
+    End Sub
+
+    Private Sub button_Click(sender As Object, e As EventArgs) Handles button.Click
+        Dim exists As Boolean
+
+        If _name.Text.Trim = "Name" Or
+                _surname.Text.Trim = "Surname" Or
+                _email.Text.Trim = "Email" Or
+                _username.Text.Trim = "Username" Or
+                _password.Text.Trim = "Password" Then
+
+            MessageBox.Show("Unfilled spaces left.",
+                            "Error",
+                            MessageBoxButtons.OK)
+        Else
+            exists = validator.validarDatos(_username.Text.Trim, _password.Text.Trim, _email.Text.Trim)
+
+            'validmail = validator.validarMail(_email.Text.Trim)
+
+            'If validmail = True Then
+            '    MsgBox("mail válido")
+            'End If
+        End If
     End Sub
 End Class
--- League_of_Champs/validador.vb
+++ League_of_Champs/validador.vb
@@ -1,8 +1,9 @@
 ﻿Public Class validador
     Public user As String = "test"
     Public pass As String = "1234"
+    Public email As String = ""
 
-    Public Function validarDatos() As Boolean
+    Public Overloads Function validarDatos() As Boolean
         validarDatos = False
 
         If inicio._username.Text.Trim = user And
@@ -14,5 +15,48 @@
         Return validarDatos
     End Function
 
+    Public Overloads Function validarDatos(username, password, email) As Boolean
 
+
+        Return validarDatos
+    End Function
+
+    Public Function validarMail(mail As String) As Boolean
+        validarMail = True
+
+
+        Dim x As String
+        Dim y, p, arr, ca, cp As Integer
+
+
+        ca = 0
+        cp = 0
+
+        For i = 1 To Len(mail)
+            x = GetChar(mail, i)
+            y = Asc(x)
+
+            If y = 64 Then
+                arr = i
+                ca = ca + 1
+            End If
+
+            If y = 46 Then
+                p = i
+                cp = cp + 1
+            End If
+        Next
+
+        If arr < 2 Or ca <> 1 Then
+            validarMail = False
+        End If
+
+        If validarMail = True Then
+            If p + 1 < arr Or cp <> 1 Or (Len(mail) - 3) < p Then
+                validarMail = False
+            End If
+        End If
+
+        Return validarMail
+    End Function
 End Class
